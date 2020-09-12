0194e480304f27c704e54908d80a955432327740
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-09-12 11:38:49 +0600

    end

--- ConsoleThreadKeypress/Program.cs
+++ ConsoleThreadKeypress/Program.cs
@@ -10,6 +10,36 @@ namespace ConsoleThreadKeypress
     {
         static void Main(string[] args)
         {
+            bool pause = false;
+            bool clean = false;
+
+            while (true)
+            {
+                Console.WriteLine("Тик-Так");
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.P) pause = true;
+                    else if (keyInfo.Key == ConsoleKey.R) pause = false;
+                    else if (keyInfo.Key == ConsoleKey.C) clean = true;
+                    else break;
+
+                    if (pause)
+                        Console.WriteLine("Paused.");
+                    else
+                        Console.WriteLine("Doing stuff.");
+
+                    if (clean)
+                    {
+                        Console.Clear();
+                    }
+
+                    System.Threading.Thread.Sleep(1000);
+                    
+                }
+
+                System.Threading.Thread.Sleep(1000);
+            }
         }
     }
 }
